77184a517f52515bb3dbe21245e84ac68b380f61
cr7goat-design <ppir99@aol.com>
2024-12-01 17:58:52 +0000

    Create satellite.py

---
+++ satellite.py
@@ -0,0 +1,30 @@
+import pgzrun 
+from random import randint
+from time import time 
+
+WIDTH = 600
+HEIGHT = 500 
+
+satellites = [] 
+lines = [] 
+next_satallite = 0
+
+start_time = 0 
+total_time = 0 
+end_time = 0 
+
+number_of_satellite = 10 
+
+def create_satallite():
+    pass
+
+def draw():
+    pass
+
+def update(): 
+    pass
+
+def on_mouse_down(pos): 
+    pass 
+
+create_satallite() 
