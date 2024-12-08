96cc42ed5e8a71a85098ce2c281763d245e77cfc
cr7goat-design <ppir99@aol.com>
2024-12-08 18:21:27 +0000

    Update satellite.py

--- satellite.py
+++ satellite.py
@@ -16,15 +16,54 @@ end_time = 0
 number_of_satellite = 10 
 
 def create_satallite():
-    pass
+    global start_time 
+    for count in range(0,number_of_satellite): 
+        satellite = Actor("satellite") 
+        satellite.pos = randint(40,WIDTH-40), randint(40,HEIGHT-40) 
+        satellites.append(satellite) 
+    start_time = time() # To get current time of your computer 
+ 
+def draw(): 
+    global total_time 
+    screen.blit("background", (0,0)) 
+    number = 1
+    for satellite in satellites: 
+        screen.draw.text(str(number),(satellite.pos[0],satellite.pos[1]+20)) 
+        satellite.draw() 
+        number += 1
+    for line in lines:
+        screen.draw.line(line[0], line[1], (255,255,255)) 
+        if next_satallite < number_of_satellite: 
+            total_time = time() - start_time 
+            screen.draw.text(str(round(total_time,1)), (10,10), fontsize=30) 
+        else: 
+            screen.draw.text(str(round(total_time,1)), (10,10), fontsize=30) 
 
-def draw():
-    pass
+
+
+
+
+
 
 def update(): 
     pass
 
 def on_mouse_down(pos): 
-    pass 
+    global lines,next_satallite  
+    if next_satallite < number_of_satellite:
+        if satellites[next_satallite].collidepoint(pos): 
+            if next_satallite:
+                lines.append((satellites[next_satallite-1].pos,satellites[next_satallite].pos)) 
+            next_satallite +=1  
+        else: 
+            line = [] 
+            next_satallite = 0
+
 
 create_satallite() 
+
+
+
+
+
+pgzrun.go()
